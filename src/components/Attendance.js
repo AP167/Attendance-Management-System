32ec91d2b67a4b6a3593c9cfb2468445fd3d0fb4
Anoushka <anoushka.patra@gmail.com>
2022-05-29 16:27:11 +0530

    Added face detection

--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -4,11 +4,31 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import Webcam from 'react-webcam'
+import * as faceapi from 'face-api.js'
+import './CanvasPos.css'
 
 const videoConstraints = {
     width: 720,
     height: 560,
     facingMode: "user"
+}
+
+function loadXHR(url) {
+
+    return new Promise(function(resolve, reject) {
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+            xhr.responseType = "blob";
+            xhr.onerror = function() {reject("Network error.")};
+            xhr.onload = function() {
+                if (xhr.status === 200) {resolve(xhr.response)}
+                else {reject("Loading error:" + xhr.statusText)}
+            };
+            xhr.send();
+        }
+        catch(err) {reject(err.message)}
+    });
 }
 
 const Attendance = () => {
@@ -29,13 +49,46 @@ const Attendance = () => {
 
 
     // console.log(currentUser)
-    if (!currentUser)
+    if (!currentUser){
         return (
         <div>You cannot access this page. Return to 
             <span onClick={() => navigate('/')}
             style={{color: "coral", cursor: "pointer"}}> Home Page</span>
-        </div>)
+        </div>)}
 
+    let canvas = null
+    const getCanvas = () => {
+        console.log(image)
+        Promise.all([
+            faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+            faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+            faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+          ]).then(() => {loadXHR(image)
+                .then((blob) => {
+                    faceapi.bufferToImage(blob)
+                    .then((bimage) => {
+                        canvas = faceapi.createCanvasFromMedia(bimage)
+                        document.getElementById('webcam-img').append(canvas)
+                        const displaySize = { width: bimage.width, height: bimage.height }
+                        faceapi.matchDimensions(canvas, displaySize)
+                        faceapi.detectAllFaces(bimage).withFaceLandmarks().withFaceDescriptors()
+                        .then((detections) => {
+                            const resizedDetections = faceapi.resizeResults(detections, displaySize)
+                            resizedDetections.forEach(detection => {
+                                const box = detection.detection.box
+                                const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' })
+                                drawBox.draw(canvas)
+                            })
+                        })
+                    })
+                    .catch((err) => {console.log(err)})
+                })
+            })
+        
+        
+        
+        return <></>
+    }
 
     return (
         <Container
@@ -54,7 +107,8 @@ const Attendance = () => {
                     <h2 className="text-center mb-4">Attendance</h2>
                     
                     <div className="webcam-container">
-                        <div className="webcam-img">
+                        <div className="webcam-img" id='webcam-img'>
+                            {image === '' ? <></> : getCanvas()}
 
                             {image === '' ? <Webcam
                                 audio={false}
@@ -83,7 +137,19 @@ const Attendance = () => {
                                     <Button  
                                     onClick={(e) => {
                                         e.preventDefault()
+                                        console.log("canvas  " + canvas)
+                                        if (canvas) {
+                                            const items = document.querySelectorAll("canvas");
+                                            for (let item of items) {
+                                                item.remove();
+                                            }
+                                            // console.log("clearing canvas")
+                                            // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+                                            // canvas.remove()
+                                        }
                                         setImage('')
+
+                                        
                                     }}
                                     style={{width: "400px", marginTop: "15px"}}>
                                         Retake Image
